855d45c1bd722b03304d8ea6c28e897f9aa96fb7
lucasdclopesspd <lucas.lopes@speedyofficer.com.br>
2021-11-11 13:13:09 -0300

    Criando esqueleto do projeto

---
+++ src/test/java/br/univesp/diarioclassedigital/DiarioClasseDigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.univesp.diarioclassedigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiarioClasseDigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
