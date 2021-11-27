7a6ea9f9b48e5b9d299ac8427cfd55721025bfbb
lucasdclopes <lucas.dc.lopes@gmail.com>
2021-11-27 19:21:31 -0300

    Adicionando mais alunos

--- src/main/java/br/univesp/diarioclasse/PopularBase.java
+++ src/main/java/br/univesp/diarioclasse/PopularBase.java
@@ -5,6 +5,7 @@ import java.time.LocalTime;
 import java.time.temporal.ChronoUnit;
 import java.util.Arrays;
 import java.util.Optional;
+import java.util.Random;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.ContextRefreshedEvent;
@@ -81,37 +82,54 @@ public class PopularBase  {
 		turmaDao.saveAll(Arrays.asList(turmaFundamental1,turmaFundamental2,turmaFundamental3,turmaFundamental4,turmaFundamental5,turmaFundamental6,
 				turmaFundamental7,turmaFundamental8,turmaFundamental9,turmaFundamental10,turmaFundamental11,turmaFundamental12,turmaFundamental13,
 				turmaFundamental14,turmaMedioB,turmaMedioA,turmaMedio2B,turmaMedio2A,turmaMedio3B,turmaMedio3A));
-		
+	
 		
 		Aluno lex = new Aluno("123456",LocalDate.now(),"11111", Optional.of(turmaFundamental5), "Lex Murphy", "46001471037", "46001471037", LocalDate.now(), 
 				Sexo.FEMININO, "Mãe do Joãozinho", "Pai do Joãozinho","teste@teste.com.br");
-		Aluno Tim = new Aluno("123457",LocalDate.now(),"11112", Optional.of(turmaFundamental5), "Tim Murphy", "66881503016", "66881503016", LocalDate.now(), 
+		Aluno Tim = new Aluno("123457",LocalDate.now(),"36960", Optional.of(turmaFundamental5), "Tim Murphy", "66881503016", "66881503016", LocalDate.now(), 
+				Sexo.MASCULINO, "Mãe do Joãozinho", "Pai do Joãozinho","teste@teste.com.br");	
+		Aluno goku = new Aluno("884800",LocalDate.now(),"31213", Optional.of(turmaFundamental5), "Son Goku", "03413572008", "03413572008", LocalDate.now(), 
+				Sexo.MASCULINO, "Son Gine", "Bardock","teste@teste.com.br");
+		Aluno magali = new Aluno("934214",LocalDate.now(),"19349", Optional.of(turmaFundamental5), "Magali de Lima", "35755796017", "35755796017", LocalDate.now(), 
+				Sexo.FEMININO, "Mãe da Magali", "Pai da Magali","teste@teste.com.br");		
+		Aluno monica = new Aluno("450522",LocalDate.now(),"53147", Optional.of(turmaFundamental5), "Monica de Souza", "23752729007", "23752729007", LocalDate.now(), 
+				Sexo.FEMININO, "Mãe da Monica", "Pai da Moncia","teste@teste.com.br");
+		Aluno harry = new Aluno("551558",LocalDate.now(),"93129", Optional.of(turmaFundamental5), "Harry Potter", "62929668016", "62929668016", LocalDate.now(), 
+				Sexo.MASCULINO, "Lílian Evans Potter", "James Potter","teste@teste.com.br");		
+		Aluno bobesp = new Aluno("895694",LocalDate.now(),"30811", Optional.of(turmaFundamental5), "Bob Esponja", "19416395005", "19416395005", LocalDate.now(), 
+				Sexo.FEMININO, "Mãe do bob", null,"teste@teste.com.br");
+		Aluno anakin = new Aluno("869980",LocalDate.now(),"22302", Optional.of(turmaFundamental5), "Anakin Skywalker", "61100839097", "61100839097", LocalDate.now(), 
+				Sexo.MASCULINO, "Shmi Skywalker", null,"teste@teste.com.br");		
+		Aluno luke = new Aluno("528903",LocalDate.now(),"53061", Optional.of(turmaFundamental5), "Luke Skywalker", "18810973070", "18810973070", LocalDate.now(), 
+				Sexo.MASCULINO, "Padmé Amidala", "Anakin Skywalker","teste@teste.com.br");
+		Aluno gohan = new Aluno("777933",LocalDate.now(),"39412", Optional.of(turmaFundamental5), "Son Gohan", "86198992012", "86198992012", LocalDate.now(), 
+				Sexo.MASCULINO, "Tchi Tchi", "Son Goku","teste@teste.com.br");
+		
+		Aluno Dennis = new Aluno("429599",LocalDate.now(),"63275", Optional.of(turmaMedioA), "Dennis Nedry", "33017780031", "33017780031", LocalDate.now(), 
 				Sexo.MASCULINO, "Mãe do Joãozinho", "Pai do Joãozinho","teste@teste.com.br");
-		Aluno Dennis = new Aluno("123458",LocalDate.now(),"11113", Optional.of(turmaMedioA), "Dennis Nedry", "33017780031", "33017780031", LocalDate.now(), 
-				Sexo.MASCULINO, "Mãe do Joãozinho", "Pai do Joãozinho","teste@teste.com.br");
-		Aluno Apoc = new Aluno("123459",LocalDate.now(),"123459", Optional.of(turmaMedioA), "Apoc Arahanga", "31455317004", "31455317004", LocalDate.now(), 
+		Aluno Apoc = new Aluno("428133",LocalDate.now(),"14474", Optional.of(turmaMedioA), "Apoc Arahanga", "31455317004", "31455317004", LocalDate.now(), 
 				Sexo.MASCULINO, "Mãe dele", null,"teste@teste.com.br");
-		Aluno DuJour = new Aluno("123460",LocalDate.now(),"123460", Optional.of(turmaMedioA), "DuJour Gray", "83885605066", "83885605066", LocalDate.now(), 
+		Aluno DuJour = new Aluno("870342",LocalDate.now(),"18583", Optional.of(turmaMedioA), "DuJour Gray", "83885605066", "83885605066", LocalDate.now(), 
 				Sexo.FEMININO, "Mãe", "Pai","teste@teste.com.br");
-		Aluno Cypher = new Aluno("123461",LocalDate.now(),"123461", Optional.of(turmaMedioA), "Cypher Reagan", "31504681088", "31504681088", LocalDate.now(), 
+		Aluno Cypher = new Aluno("942264",LocalDate.now(),"67104", Optional.of(turmaMedioA), "Cypher Reagan", "31504681088", "31504681088", LocalDate.now(), 
 				Sexo.DESCONHECIDO, "Mãe", "Pai","teste@teste.com.br");
-		Aluno Dozer = new Aluno("123462",LocalDate.now(),"123462", Optional.of(turmaMedioA), "Dozer Nebuchadnezzar", "35764261082", "35764261082", LocalDate.now(), 
+		Aluno Dozer = new Aluno("109747",LocalDate.now(),"14190", Optional.of(turmaMedioA), "Dozer Nebuchadnezzar", "35764261082", "35764261082", LocalDate.now(), 
 				Sexo.MASCULINO, "Mãe", "Pai","teste@teste.com.br");
-		Aluno Mouse = new Aluno("123463",LocalDate.now(),"123463", Optional.of(turmaMedioA), "Mouse Nebuchadnezzar", "25730251017", "25730251017", LocalDate.now(), 
+		Aluno Mouse = new Aluno("509527",LocalDate.now(),"87823", Optional.of(turmaMedioA), "Mouse Nebuchadnezzar", "25730251017", "25730251017", LocalDate.now(), 
 				Sexo.MASCULINO, "Mãe", null,"teste@teste.com.br");
-		Aluno Rhineheart = new Aluno("123464",LocalDate.now(),"123464", Optional.of(turmaMedioA), "Rhineheart Metacortex", "26370561002", "26370561002", LocalDate.now(), 
+		Aluno Rhineheart = new Aluno("919831",LocalDate.now(),"89153", Optional.of(turmaMedioA), "Rhineheart Metacortex", "26370561002", "26370561002", LocalDate.now(), 
 				Sexo.MASCULINO, "Mãe", null,"teste@teste.com.br");
-		Aluno Switch = new Aluno("123465",LocalDate.now(),"123465", Optional.of(turmaMedioA), "Switch McClory", "31132802067", "31132802067", LocalDate.now(), 
+		Aluno Switch = new Aluno("928651",LocalDate.now(),"84311", Optional.of(turmaMedioA), "Switch McClory", "31132802067", "31132802067", LocalDate.now(), 
 				Sexo.FEMININO, "Mãe", null,"teste@teste.com.br");
-		Aluno Tank = new Aluno("123466",LocalDate.now(),"123466", Optional.of(turmaFundamental14), "Tank Chong", "68529544048", "68529544048", LocalDate.now(), 
+		Aluno Tank = new Aluno("757138",LocalDate.now(),"47166", Optional.of(turmaFundamental14), "Tank Chong", "68529544048", "68529544048", LocalDate.now(), 
 				Sexo.MASCULINO, "Mãe", null,"teste@teste.com.br");
-		Aluno Trinity = new Aluno("123467",LocalDate.now(),"123467", Optional.of(turmaFundamental7), "Trinity Moss", "92475725095", "92475725095", LocalDate.now(), 
+		Aluno Trinity = new Aluno("296191",LocalDate.now(),"94646", Optional.of(turmaFundamental7), "Trinity Moss", "92475725095", "92475725095", LocalDate.now(), 
 				Sexo.FEMININO, "Mãe", null,"teste@teste.com.br");
-		Aluno Donald = new Aluno("123468",LocalDate.now(),"123468", Optional.of(turmaFundamental14), "Donald Gennaro", "55904133030", "55904133030", LocalDate.now(), 
+		Aluno Donald = new Aluno("573533",LocalDate.now(),"10923", Optional.of(turmaFundamental14), "Donald Gennaro", "55904133030", "55904133030", LocalDate.now(), 
 				Sexo.MASCULINO, "Mãe", null,"teste@teste.com.br");
 		
 		alunoDao.saveAll(Arrays.asList(
-				lex,Tim,Dennis,Apoc,DuJour,Cypher,Dozer,Mouse,Rhineheart,Switch,Tank,Trinity,Donald
+				lex,Tim,Dennis,Apoc,DuJour,Cypher,Dozer,Mouse,Rhineheart,Switch,Tank,Trinity,Donald,goku,magali,monica,harry,bobesp,anakin,luke,gohan
 				));
 		
 		Materia matF1 = new Materia("Matemática",TipoNivelEnsino.FUNDAMENTAL_II);
